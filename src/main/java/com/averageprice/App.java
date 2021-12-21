ed4a6aa1421f04987e8fdd5dd231b779d9bd7ff6
Caio <caiofellipe80@gmail.com>
2021-12-20 22:00:59 -0300

    maven install

---
+++ src/main/java/com/averageprice/App.java
@@ -0,0 +1,12 @@
+package com.averageprice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
